d1918fc1587dd68ce4095454a46fed3ad8965f07
Seppo Miranda <alsemila@gmail.com>
2020-03-06 01:34:13 +0200

    paivituksia

--- sw_cached_site.js
+++ sw_cached_site.js
@@ -6,24 +6,16 @@ const cacheAssets = [
     'opiskelut.html',
     'main.js',
     '/css/main.css',
-    '/images/kappaleet/meilleAurinkoPaistaa.PNG',
-    '/images/kappaleet/meilleAurinkoPaistaa2&3.PNG',
-    '/images/kappaleet/samaTaivas-samaMaa1.PNG',
-    '/images/kappaleet/samaTaivas-samaMaa2&3.PNG',
-    '/images/kappaleet/tartuKateenJumalan1.PNG',
-    '/images/kappaleet/tartuKateenJumalan2&3.PNG',
-    '/images/kappaleet/tuleNuotionLoimuun1.PNG',
-    '/images/kappaleet/tuleNuotionLoimuun2&3.PNG',
     '/images/kuvakkeet/ig-color.svg',
     '/images/kuvakkeet/kirja.svg',
     '/images/kuvakkeet/logo.svg',
     '/images/kuvakkeet/logoB.svg',
     '/images/kuvakkeet/nuotti.svg',
-    '/images/kuvakkeet/koti.svg',
+    '/images/kuvakkeet/koti.svg'
 ]
 
 // Call Install Event
-self.addEventListener('install', e => {
+self.addEventListener('install', (e) => {
     console.log('Service Worker: Installed');
   
     e.waitUntil(
@@ -38,11 +30,11 @@ self.addEventListener('install', e => {
   });
   
   // Call Activate Event
-  self.addEventListener('activate', e => {
+  self.addEventListener('activate', (e) => {
     console.log('Service Worker: Activated');
     // Remove unwanted caches
     e.waitUntil(
-      caches.keys().then(cacheNames => {
+      caches.keys().then((cacheNames) => {
         return Promise.all(
           cacheNames.map(cache => {
             if (cache !== cacheName) {
@@ -56,7 +48,7 @@ self.addEventListener('install', e => {
   });
   
   // Call Fetch Event
-  self.addEventListener('fetch', e => {
+  self.addEventListener('fetch', (e) => {
     console.log('Service Worker: Fetching');
     e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
   });  
