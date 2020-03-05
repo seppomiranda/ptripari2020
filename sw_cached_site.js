a09802866d206ffb269db68c0b7b2c6a1196995a
Seppo Miranda <alsemila@gmail.com>
2020-03-06 00:17:42 +0200

    uudelleen

--- sw_cached_site.js
+++ sw_cached_site.js
@@ -1,25 +1,25 @@
 const cacheName = 'ptripari2020';
 
 const cacheAssets = [
-    './index.html',
-    './laulut.html',
-    './opiskelut.html',
-    './main.js',
-    './css/main.css',
-    './images/kappaleet/meilleAurinkoPaistaa.PNG',
-    './images/kappaleet/meilleAurinkoPaistaa2&3.PNG',
-    './images/kappaleet/samaTaivas-samaMaa1.PNG',
-    './images/kappaleet/samaTaivas-samaMaa2&3.PNG',
-    './images/kappaleet/tartuKateenJumalan1.PNG',
-    './images/kappaleet/tartuKateenJumalan2&3.PNG',
-    './images/kappaleet/tuleNuotionLoimuun1.PNG',
-    './images/kappaleet/tuleNuotionLoimuun2&3.PNG',
-    './images/kuvakkeet/ig-color.svg',
-    './images/kuvakkeet/kirja.svg',
-    './images/kuvakkeet/logo.svg',
-    './images/kuvakkeet/logoB.svg',
-    './images/kuvakkeet/nuotti.svg',
-    './images/kuvakkeet/koti.svg',
+    'index.html',
+    'laulut.html',
+    'opiskelut.html',
+    'main.js',
+    'css/main.css',
+    'images/kappaleet/meilleAurinkoPaistaa.PNG',
+    'images/kappaleet/meilleAurinkoPaistaa2&3.PNG',
+    'images/kappaleet/samaTaivas-samaMaa1.PNG',
+    'images/kappaleet/samaTaivas-samaMaa2&3.PNG',
+    'images/kappaleet/tartuKateenJumalan1.PNG',
+    'images/kappaleet/tartuKateenJumalan2&3.PNG',
+    'images/kappaleet/tuleNuotionLoimuun1.PNG',
+    'images/kappaleet/tuleNuotionLoimuun2&3.PNG',
+    'images/kuvakkeet/ig-color.svg',
+    'images/kuvakkeet/kirja.svg',
+    'images/kuvakkeet/logo.svg',
+    'images/kuvakkeet/logoB.svg',
+    'images/kuvakkeet/nuotti.svg',
+    'images/kuvakkeet/koti.svg',
 ]
 
 // Call Install Event
@@ -50,7 +50,7 @@ self.addEventListener('activate', (e) => {
                         return caches.delete(cache);
                     }
                 })
-            )
+            );
         })
     );
 });
@@ -59,6 +59,6 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', e => {
     console.log('Service worker: Fetching');
     e.respondWith(
-        fetch(e.request).catch(()=> caches.match(e.request))
+        fetch(e.request).catch(() => caches.match(e.request))
     )
 })
